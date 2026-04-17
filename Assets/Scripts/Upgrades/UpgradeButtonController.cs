6b41f9faa6ae0f874f42c458942edd8070bb3dc0
potato <potato@potatogam.es>
2026-04-17 07:45:56 +0200

    Navigation fix for doubled input

--- Assets/Scripts/Upgrades/UpgradeButtonController.cs
+++ Assets/Scripts/Upgrades/UpgradeButtonController.cs
@@ -1,46 +1,6 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-
-public class UpgradeButtonController : MonoBehaviour, ISelectHandler, IDeselectHandler
+public class UpgradeButtonController : MonoBehaviour
 {
-    [SerializeField] private UpgradePanelController upgradePanel;
-    [SerializeField] private bool defaultOnEnable;
-    [SerializeField] private MenuSoundManager soundManager;
-
-    void OnEnable()
-    {
-        if (defaultOnEnable && EventSystem.current.currentSelectedGameObject != gameObject)
-        {
-            EventSystem.current.SetSelectedGameObject(null);
-            EventSystem.current.SetSelectedGameObject(gameObject);
-        }
-    }
-
-    void OnDisable()
-    {
-        if (EventSystem.current.currentSelectedGameObject != null && EventSystem.current.currentSelectedGameObject == gameObject)
-        {
-            // Manually, because if the button gets disabled, OnDeselect does not run.
-            EventSystem.current.SetSelectedGameObject(null);
-
-            upgradePanel.SetSelected(false);
-        }
-    }
-
-    // This does not get called when the button gets disabled!
-    // BUT! It does get called when the button changes to non-interactable.
-    // FFFFFFFFuuuuuuuuuuuuuuuuuuuuuuuuu...
-    public void OnDeselect(BaseEventData eventData)
-    {
-        upgradePanel.SetSelected(false);
-    }
-
-    public void OnSelect(BaseEventData eventData)
-    {
-        upgradePanel.SetSelected(true);
-
-        soundManager.PlaySelect();
-    }
 
 }
