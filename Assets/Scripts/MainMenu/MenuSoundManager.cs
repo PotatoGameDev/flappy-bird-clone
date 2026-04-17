b3fea65cd2da4424ae72a55b83ea58f8a0d6154a
potato <potato@potatogam.es>
2026-04-17 11:35:39 +0200

    Menu music queue

--- Assets/Scripts/MainMenu/MenuSoundManager.cs
+++ Assets/Scripts/MainMenu/MenuSoundManager.cs
@@ -1,17 +1,43 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
 public class MenuSoundManager : MonoBehaviour
 {
+    private readonly WaitForSeconds WAIT_ONE_SEC = new(1);
+
     [SerializeField] private AudioClip clickClip;
     [SerializeField] private AudioClip upgradeClip;
     [SerializeField] private AudioClip selectClip;
+    [SerializeField] private AudioClip[] musicClips;
 
     private AudioSource audioSource;
 
     void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+
+        StartCoroutine(PlayRandomMusic());
+    }
+
+    public IEnumerator PlayRandomMusic()
+    {
+        int currentSong = Random.Range(0, musicClips.Length);
+        while (true)
+        {
+            if (audioSource.isPlaying)
+            {
+                yield return WAIT_ONE_SEC;
+            }
+            else
+            {
+                audioSource.clip = musicClips[currentSong % musicClips.Length];
+                audioSource.Play();
+
+                yield return new WaitForSeconds(audioSource.clip.length);
+                currentSong++;
+            }
+        }
     }
 
     public void PlayClick()
